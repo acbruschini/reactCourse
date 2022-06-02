666e63490fd6e7b5d8925fb1eb9eac4479601a03
Ariel Bruschini <bruschini@gmail.com>
2022-06-02 16:59:41 -0300

    Desafio 03

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/NavBar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <ItemListContainer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './img/cart-shopping-solid.svg';
+
+function CartWidget() {
+  return (
+    <img src={logo} style={{width: '25px'}}/>
+  )
+}
+
+export default CartWidget
